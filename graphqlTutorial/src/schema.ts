2da8f37ec655726de04abd0eb334aaefdd55d637
syahmifouzi <syahmi.fouzi@gmail.com>
2024-05-16 18:25:43 +0800

    added ability to take a number when query

--- graphqlTutorial/src/schema.ts
+++ graphqlTutorial/src/schema.ts
@@ -1,15 +1,23 @@
 import { createSchema } from 'graphql-yoga'
-import { type Link } from '@prisma/client'
+import { type Link, type SensorData } from '@prisma/client'
 import type { GraphQLContext } from './context'
 
 const typeDefinitions = /* GraphQL */ `
     type Query {
         info: String!
         feed: [Link!]!
+        sensorData(take: Int): [SensorData!]!
     }
 
     type Mutation {
         postLink(url: String!, description: String!): Link!
+        postSensorData(value: Float!): SensorData!
+    }
+
+    type SensorData {
+        id: ID!
+        value: Float!
+        createdAt: String!
     }
 
     type Link {
@@ -22,7 +30,17 @@ const typeDefinitions = /* GraphQL */ `
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: async (parent: unknown, args: {}, context: GraphQLContext) => context.prisma.link.findMany()
+        feed: async (parent: unknown, args: {}, context: GraphQLContext) => context.prisma.link.findMany(),
+        async sensorData (parent: unknown, args: {take?: number}, context: GraphQLContext) {
+            const take = args.take
+            return context.prisma.sensorData.findMany({
+                take
+            })
+        }
+    },
+    SensorData: {
+        id: (parent: SensorData) => parent.id,
+        value: (parent: SensorData) => parent.value,
     },
     Link: {
         id: (parent: Link) => parent.id,
@@ -38,7 +56,15 @@ const resolvers = {
                 }
             })
             return newLink
-        }
+        },
+        async postSensorData(parent: unknown, args: { value: number }, context: GraphQLContext) {
+            const newData = await context.prisma.sensorData.create({
+                data: {
+                    value: args.value
+                }
+            })
+            return newData
+        },
     }
 }
 
